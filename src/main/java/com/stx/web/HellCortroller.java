0264c482525e01e2243701d64cff04e5ecc97472
Yangheng1517 <574874733@qq.com>
2019-06-11 00:08:41 +0800

    第一次修改

--- src/main/java/com/stx/web/HellCortroller.java
+++ src/main/java/com/stx/web/HellCortroller.java
@@ -1,7 +1,15 @@
 package com.stx.web;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.stx.biz.UserBiz;
+import com.stx.entity.user;
 
 /**
 *@author yh
@@ -11,9 +19,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 */
 @Controller
 public class HellCortroller {
-	@RequestMapping("/hello.do")
-	public String hello() {
-		System.out.println("hello spring...");
-		return "hello";
+	@Autowired
+	private UserBiz userBiz;
+	
+	
+	
+	@RequestMapping(value="/login.do",method=RequestMethod.POST)
+	public ModelAndView login(user user,HttpSession session){
+		user myUser = userBiz.login(user);
+		ModelAndView mav1 = new ModelAndView("index");
+		ModelAndView mav2 = new ModelAndView("login","Error","登录失败");
+		if(myUser!=null){
+			session.setAttribute("USER", myUser);
+			return mav1;
+		}
+		return mav2;
 	}
 }
